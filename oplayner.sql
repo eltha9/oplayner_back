fa0e87d7c89ba22a529f5df472cab4b49a924eec
fxmanceau <francois-xavier@MacBook-Pro-de-Francois-Xavier.local>
2019-06-01 00:54:03 +0200

    :art: Rename tables

--- oplayner.sql
+++ oplayner.sql
@@ -2,10 +2,10 @@
 -- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:8889
--- Generation Time: May 31, 2019 at 11:40 AM
--- Server version: 5.7.23
--- PHP Version: 7.2.10
+-- Hôte : localhost:8889
+-- Généré le :  ven. 31 mai 2019 à 22:52
+-- Version du serveur :  5.7.23
+-- Version de PHP :  7.2.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,16 +17,16 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `oplayner`
+-- Base de données :  `oplayner`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `musique`
+-- Structure de la table `musics`
 --
 
-CREATE TABLE `musique` (
+CREATE TABLE `musics` (
   `id` int(11) NOT NULL,
   `id_party` int(11) NOT NULL,
   `content` json NOT NULL
@@ -35,99 +35,107 @@ CREATE TABLE `musique` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `party`
+-- Structure de la table `parties`
 --
 
-CREATE TABLE `party` (
+CREATE TABLE `parties` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `time` int(11) NOT NULL,
   `description` text NOT NULL,
-  `adresse` text NOT NULL,
-  `telephone` int(11) NOT NULL
+  `adress` text NOT NULL,
+  `phone` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `produit`
+-- Structure de la table `products`
 --
 
-CREATE TABLE `produit` (
+CREATE TABLE `products` (
   `id` int(11) NOT NULL,
   `id_party` int(11) NOT NULL,
   `content` json NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Déchargement des données de la table `products`
+--
+
+INSERT INTO `products` (`id`, `id_party`, `content`) VALUES
+(2, 123, '{\"product\": \"zerg\"}');
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Structure de la table `users`
 --
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `login` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
   `avatar_id` int(11) NOT NULL,
   `id_party` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Indexes for dumped tables
+-- Index pour les tables déchargées
 --
 
 --
--- Indexes for table `musique`
+-- Index pour la table `musics`
 --
-ALTER TABLE `musique`
+ALTER TABLE `musics`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `party`
+-- Index pour la table `parties`
 --
-ALTER TABLE `party`
+ALTER TABLE `parties`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `produit`
+-- Index pour la table `products`
 --
-ALTER TABLE `produit`
+ALTER TABLE `products`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `users`
+-- Index pour la table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT pour les tables déchargées
 --
 
 --
--- AUTO_INCREMENT for table `musique`
+-- AUTO_INCREMENT pour la table `musics`
 --
-ALTER TABLE `musique`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `musics`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `party`
+-- AUTO_INCREMENT pour la table `parties`
 --
-ALTER TABLE `party`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `parties`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `produit`
+-- AUTO_INCREMENT pour la table `products`
 --
-ALTER TABLE `produit`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
