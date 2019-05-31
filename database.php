5f84336c363266108464db3e3306d3d556562432
MattyxBaguette <mathis.gadie@hetic.net>
2019-05-31 14:25:50 +0200

    :card_file_box: database.php

---
+++ database.php
@@ -0,0 +1,13 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_PORT', '8889');
+define('DB_NAME', 'oplayner');
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
+try {
+    $bdd = new PDO('mysql:dbname='.DB_NAME.';host='.DB_HOST.';port='.DB_PORT, DB_USER, DB_PASS);
+    $bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+}
+catch (PDOException $e) {
+    die('Cannot connect');
+}
