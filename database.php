3134b958119bb77ad207fbc3f5e8aec1e6654486
fxmanceau <francois-xavier@MacBook-Pro-de-Francois-Xavier.local>
2019-06-01 00:54:51 +0200

    :wrench: Rconfigure database

--- database.php
+++ database.php
@@ -5,8 +5,8 @@ define('DB_NAME', 'oplayner');
 define('DB_USER', 'root');
 define('DB_PASS', 'root');
 try {
-    $bdd = new PDO('mysql:dbname='.DB_NAME.';host='.DB_HOST.';port='.DB_PORT, DB_USER, DB_PASS);
-    $bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+    $pdo = new PDO('mysql:dbname='.DB_NAME.';host='.DB_HOST.';port='.DB_PORT, DB_USER, DB_PASS);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 }
 catch (PDOException $e) {
     die('Cannot connect');
