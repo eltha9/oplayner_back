8c813ce0d5af7c1a63a9e151a169d1a0b27874fe
eltha9 <theadosto@gmail.com>
2019-06-04 09:44:40 +0200

    up to push

--- wrapper.php
+++ wrapper.php
@@ -1,16 +1,16 @@
 <?php
     include 'database.php';
 
+    error_log('post value = '.json_encode($_POST), 0);
+
     $result = new stdClass();
     
     if(!empty($_POST))
     {
-        echo '<pre>';
-        var_dump($_POST);
-        echo '<pre>';
 
         if(!empty($_POST['get']) && $_POST['get'] == 'true')
         {
+            error_log('tabke value = '.json_encode($_POST['table']), 0);
             if($_POST['table'] == 'user')
             {
                 $prepare = $pdo->prepare('SELECT * FROM users WHERE id = :equalTo');
@@ -97,6 +97,7 @@
         }
         else if(!empty($_POST['post']) && $_POST['post'] == 'true')
         {
+            error_log('tabke value = '.json_encode($_POST['table']), 0);
             if($_POST['table'] == 'user')
             {
                 $prepare = $pdo->prepare('INSERT INTO users (login, password, avatar_id, id_party) VALUES (:login, :password, :avatar_id, :id_party)');
@@ -120,7 +121,9 @@
                     "adresse" => $_POST['adresse'],
                     "telephone" => $_POST['telephone']
                 ];
-                $prepare->execute($data);
+                error_log('time value = '.json_encode($data['time']), 0);
+                $error = $prepare->execute($data);
+                error_log('db value = '.print_r($error), 0);
             }
 
             else if($_POST['table'] == 'music')
@@ -144,76 +147,16 @@
             }
         }
 
-        echo '<pre>';
-        var_dump($result);
-        echo '<pre>';
+        
+        echo json_encode($result);
+        
     }
     else
     {
         $result->code = '400';
         $result->content = "bad request";
-        echo '<pre>';
-        var_dump($result);
-        echo '<pre>';
+        
+        echo json_encode($result);
+        
     }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Get content</h1>
-    <form action="#" method="post">
-        <input type="type" name="get" id="" value="true" readonly="readonly">
-        <input type="text" name="table" id="" placeholder="table">
-        <input type="text" name="equalTo" id="" placeholder="equalTo">
-        <input type="submit" value="crawl">
-    </form>
-
-    <h1>Put user</h1>
-    <form action="#" method="post">
-        <input type="type" name="post" id="" value="true" readonly="readonly">
-        <input type="text" name="table" id="" value="user" readonly="readonly">
-        <input type="text" name="login" id="" placeholder="login">
-        <input type="text" name="password" id="" placeholder="password">
-        <input type="number" name="avatar_id" id="" placeholder="id avatar">
-        <input type="text" name="id_party" id="" placeholder="id party">
-        <input type="submit" value="put user">
-    </form>
-
-    <h1>Put party</h1>
-    <form action="#" method="post">
-        <input type="type" name="post" id="" value="true" readonly="readonly">
-        <input type="text" name="table" id="" value="party" readonly="readonly">
-        <input type="text" name="name" id="" placeholder="name">
-        <input type="text" name="password" id="" placeholder="password">
-        <input type="date" name="time" id="" placeholder="time">
-        <input type="text" name="description" id="" placeholder="description">
-        <input type="text" name="adresse" id="" placeholder="adresse">
-        <input type="number" name="telephone" id="" placeholder="telephone">
-        <input type="submit" value="put party">
-    </form>
-
-    <h1>Put music</h1>
-    <form action="#" method="post">
-        <input type="type" name="post" id="" value="true" readonly="readonly">
-        <input type="text" name="table" id="" value="music" readonly="readonly">
-        <input type="text" name="id_party" id="" placeholder="id party">
-        <input type="text" name="content" id="" placeholder="json content">
-        <input type="submit" value="put music">
-    </form>
-
-    <h1>Put products</h1>
-    <form action="#" method="post">
-        <input type="type" name="post" id="" value="true" readonly="readonly">
-        <input type="text" name="table" id="" value="product" readonly="readonly">
-        <input type="text" name="id_party" id="" placeholder="id party">
-        <input type="text" name="content" id="" placeholder="json content">
-        <input type="submit" value="put product">
-    </form>
-</body>
-</html>
